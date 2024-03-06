c8778731502d6760fb45731bea9b45336a9b4a61
swiftleah <leahrosehenry@gmail.com>
2024-03-06 14:52:27 +0000

    Task 4 - works now

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,3 +1,3 @@
-export function signUpUser(firstName, lastName) {
-	return Promise.resolve({ firstName, lastName });
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
 }
