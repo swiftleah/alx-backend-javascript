db999059389671a20f9d73d8b7ce8523f9ee43f5
swiftleah <leahrosehenry@gmail.com>
2024-03-07 12:34:57 +0000

    Task 7 - output not completely correct - testing

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,26 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+  
+  toString() {
+        return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+    }
+}
