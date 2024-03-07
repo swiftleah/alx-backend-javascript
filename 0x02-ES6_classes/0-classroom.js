ba3b543f8c1941dfe172810235938e8593273f66
swiftleah <leahrosehenry@gmail.com>
2024-03-07 10:59:33 +0000

    Task 0 - added more documentation

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,8 +1,10 @@
 /*
- * ClassRoom represents a classroom
+ * Cretaes class room
  */
-
 export default class ClassRoom {
+    /*
+     * Class room
+     */
     constructor(maxStudentsSize) {
         this._maxStudentsSize = maxStudentsSize;
     }
