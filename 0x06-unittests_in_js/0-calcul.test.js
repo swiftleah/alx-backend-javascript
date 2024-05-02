6ddd41da071ff31d80e7ae94ad6a92f10f206d5a
swiftleah <leahrosehenry@gmail.com>
2024-05-02 08:37:56 +0000

    Task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,8 +8,17 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
 	it('should return sum of 2 rounded numbers', () => {
 		assert.strictEqual(calculateNumber(1, 6), 7);
+	});
+
+	it('round down a and round up b', () => {
 		assert.strictEqual(calculateNumber(2.2, 3.5), 6);
+	});
+
+	it('a whole and b round up', () => {
 		assert.strictEqual(calculateNumber(2, 5.9), 8);
+	});
+
+	it('round up a and round up b', () => {
 		assert.strictEqual(calculateNumber(1.5, 3.7), 6);
 	});
 });
