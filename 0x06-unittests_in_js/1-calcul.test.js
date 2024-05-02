7623d46a4b15e4e4f3d0715452364658483c0b7c
swiftleah <leahrosehenry@gmail.com>
2024-05-02 13:32:03 +0000

    Test fixed

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -3,89 +3,6 @@ const expect = chai.expect;
 const calculateNumber = require('./1-calcul');
 
 describe('calculateNumber tests', () => {
-<<<<<<< HEAD
-	describe('type == SUM', () => {
-		it('rounded positive', () => {
-			assert.strictEqual(calculateNumber('SUM', 2, 3), 5);
-		});
-
-		it('rounded negative', () => {
-			assert.strictEqual(calculateNumber('SUM', -2, -1), -3);
-		});
-
-		it('rounded positive and negative', () => {
-			assert.strictEqual(calculateNumber('SUM', 2, -3), -1);
-		});
-
-		it('non-rounded positive', () => {
-			assert.strictEqual(calculateNumber('SUM', 2.1, 3.9), 6);
-		});
-
-		it('non-rounded negative', () => {
-			assert.strictEqual(calculateNumber('SUM', -5.5, -9.0), -14);
-		});
-
-		it('non-rounded negative and positive', () => {
-			assert.strictEqual(calculateNumber('SUM', -2.3, 4.7), 3);
-		});
-	});
-	describe('type == SUBTRACT', () => {
-		it('rounded positive', () => {
-    			assert.strictEqual(calculateNumber('SUBTRACT', 3, 2), 1);
-		});
-
-		it('rounded negative', () => {
-    			assert.strictEqual(calculateNumber('SUBTRACT', -3, -2), -1);
-		});
-
-		it('rounded positive and negative', () => {
-    			assert.strictEqual(calculateNumber('SUBTRACT', 3, -2), 5);
-		});
-
-		it('non-rounded positive', () => {
-    			assert.strictEqual(calculateNumber('SUBTRACT', 3.9, 2.1), 2);
-		});
-
-		it('non-rounded negative', () => {
-			assert.strictEqual(calculateNumber('SUBTRACT', -9.0, -5.5), -4);
-		});
-
-		it('non-rounded negative and positive', () => {
-    			assert.strictEqual(calculateNumber('SUBTRACT', 4.7, -2.3), 7);
-		});
-
-	});
-	describe('type == DIVIDE', () => {
-		it('rounded positive', () => {
-    		assert.strictEqual(calculateNumber('DIVIDE', 6, 3), 2);
-		});
-
-		it('rounded negative', () => {
-    		assert.strictEqual(calculateNumber('DIVIDE', -6, -3), 2);
-		});
-
-		it('rounded positive and negative', () => {
-    		assert.strictEqual(calculateNumber('DIVIDE', 6, -3), -2);
-		});
-
-		it('non-rounded positive', () => {
-		    assert.strictEqual(calculateNumber('DIVIDE', 6.3, 3.2), 2);
-		});
-		
-		it('non-rounded negative', () => {
-		    assert.strictEqual(calculateNumber('DIVIDE', -6.5, -3.5), 2);
-		});
-		
-		it('non-rounded negative and positive', () => {
-		    assert.strictEqual(calculateNumber('DIVIDE', 5.5, -3.5), -2);
-		});
-		
-		it('division by zero', () => {
-		    assert.strictEqual(calculateNumber('DIVIDE', 7, 0), 'Error');
-		});
-
-	});
-=======
     describe('type == SUM', () => {
         it('rounded positive', () => {
             expect(calculateNumber('SUM', 2, 3)).to.equal(5);
@@ -165,5 +82,4 @@ describe('calculateNumber tests', () => {
             expect(calculateNumber('DIVIDE', 7, 0)).to.equal('Error');
         });
     });
->>>>>>> d8705b07aac778576c2b016a1eb783a5c62baec6
 });
